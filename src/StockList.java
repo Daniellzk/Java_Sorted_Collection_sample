0a2641a9e76100abfb9d656eefb83698450d3b52
Daniel.Z.Luo <danielluozhaokun@gmail.com>
2022-03-29 17:50:11 -0400

    add StockList Class and rename getQuantity method to QuantityInStock

---
+++ src/StockList.java
@@ -0,0 +1,57 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StockList {
+    private final Map<String, StockItem> list;
+
+    public StockList() {
+        this.list = new HashMap<>();
+    }
+
+    public int addStock(StockItem item){
+        if (item != null){
+            StockItem newItem = list.getOrDefault(item.getName(), item);
+            if (newItem != item){
+                item.adjustStock(newItem.quantityInStock());
+            }
+
+            list.put(item.getName(),item);
+            return item.quantityInStock();
+        }
+        return 0;
+    }
+
+    public int sellStock(String item, int quantity){
+        StockItem inStock = list.getOrDefault(item, null);
+        if((inStock != null) && (inStock.quantityInStock() >= quantity) && (quantity >= 0)){
+            inStock.adjustStock(-quantity);
+            return quantity;
+        }
+        return 0;
+    }
+
+    public StockItem get(String key){
+        return list.get(key);
+    }
+
+    public Map<String,StockItem> Items() {
+        return Collections.unmodifiableMap(list);
+    }
+
+    @Override
+    public String toString() {
+        String s = "\n Stock list\n";
+        double totalCost = 0.0;
+        for( Map.Entry<String, StockItem> item : list.entrySet()){
+            StockItem stockItem = item.getValue();
+
+            double itemValue = stockItem.getPrice() * stockItem.quantityInStock();
+            s = s + stockItem + ". there are " + stockItem.quantityInStock() + "in stock. Value of item: ";
+            s = s + itemValue + "\n";
+            totalCost += itemValue;
+        }
+
+        return s + "total stock value " + totalCost;
+    }
+}
